0afd4b5a86fed8c1a32bcd3f5ee1b0196357c63f
Paweł Cembaluk <241574@student.pwr.edu.pl>
2019-11-09 12:54:18 +0100

    Added basic arithmetics.

--- src/BefungeInterpreter.java
+++ src/BefungeInterpreter.java
@@ -1,11 +1,74 @@
+import java.util.Collections;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class BefungeInterpreter {
 
-    private Stack<Integer> stack;
+    private final Stack<Integer> stack = new Stack<>();
 
     public String run(String code) {
+        char[] codeArray = code.toCharArray();
+        for (char character : codeArray)
+            interpretCharacter(character);
         return "";
+    }
+
+    private void interpretCharacter(char character) {
+        if (Character.isDigit(character))
+            stack.push(Character.getNumericValue(character));
+        switch (character) {
+            case '+':
+                add();
+                break;
+            case '-':
+                subtract();
+                break;
+            case '*':
+                multiply();
+                break;
+            case '/':
+                divide();
+                break;
+            case '%':
+                modulo();
+                break;
+            case '@':
+            default:
+        }
+    }
+
+    private void add() {
+        int a = stack.pop();
+        int b = stack.pop();
+        stack.push(a + b);
+    }
+
+    private void subtract() {
+        int a = stack.pop();
+        int b = stack.pop();
+        stack.push(b - a);
+    }
+
+    private void multiply() {
+        int a = stack.pop();
+        int b = stack.pop();
+        stack.push(a * b);
+    }
+
+    private void divide() {
+        int a = stack.pop();
+        int b = stack.pop();
+        stack.push(a == 0 ? 0 : b / a);
+    }
+
+    private void modulo() {
+        int a = stack.pop();
+        int b = stack.pop();
+        stack.push(a == 0 ? 0 : b % a);
+    }
+
+    public void clearStack() {
+        stack.clear();
     }
 
     public Stack<Integer> getStack() {
--- tests/BefungeInterpreterTest.java
+++ tests/BefungeInterpreterTest.java
@@ -20,8 +20,89 @@ class BefungeInterpreterTest {
     }
 
     @Test
-    void shouldPush5OnStack() {
-        interpreter.run("5@");
-        assertEquals(5, interpreter.getStack().peek());
+    void shouldPushDigitOnStack() {
+        interpreter.run("0@");
+        assertEquals(0, interpreter.getStack().peek());
+        interpreter.clearStack();
+        interpreter.run("9@");
+        assertEquals(9, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldAddTwoDigits() {
+        interpreter.run("34+@");
+        assertEquals(7, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldAddFourDigits(){
+        interpreter.run("1111+++@");
+        assertEquals(4, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldSubtractTwoDigits() {
+        interpreter.run("43-@");
+        assertEquals(1, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldSubtractFourDigits() {
+        interpreter.run("9321---@");
+        assertEquals(7, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldMultiplyTwoDigits() {
+        interpreter.run("43*@");
+        assertEquals(12, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldMultiplyFourDigits() {
+        interpreter.run("1234***@");
+        assertEquals(24, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldDivideTwoDigits() {
+        interpreter.run("63/@");
+        assertEquals(2, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldDivideFourDigits() {
+        interpreter.run("8631///@");
+        assertEquals(4, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldDivideTwoDigitsRoundingDown() {
+        interpreter.run("83/@");
+        assertEquals(2, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldPush0ForDivisionBy0() {
+        interpreter.run("50/@");
+        assertEquals(0, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldModuloTwoDigits() {
+        interpreter.run("43%@");
+        assertEquals(1, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldModuloThreeDigits() {
+        interpreter.run("9353%%%@");
+        assertEquals(0, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldPush0ForModulo0() {
+        interpreter.run("50%@");
+        assertEquals(0, interpreter.getStack().peek());
     }
 }
