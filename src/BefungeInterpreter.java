2c9d9951c89846b7c5b9bcac33702bae331950c4
Paweł Cembaluk <241574@student.pwr.edu.pl>
2019-11-09 18:27:51 +0100

    Added string mode, stack manipulation and printing values to output.

--- src/BefungeInterpreter.java
+++ src/BefungeInterpreter.java
@@ -1,39 +1,101 @@
-import java.util.Collections;
+import java.awt.*;
 import java.util.Stack;
-import java.util.stream.Collectors;
 
 public class BefungeInterpreter {
 
     private final Stack<Integer> stack = new Stack<>();
 
+    private char[] code;
+    private Point codePointer = new Point(0, 0);
+    private boolean isStringMode = false;
+    private StringBuilder output = new StringBuilder();
+
     public String run(String code) {
-        char[] codeArray = code.toCharArray();
-        for (char character : codeArray)
-            interpretCharacter(character);
-        return "";
+        this.code = code.toCharArray();
+        while (interpretCharacter())
+            ++codePointer.x;
+        return output.toString();
     }
 
-    private void interpretCharacter(char character) {
-        if (Character.isDigit(character))
-            stack.push(Character.getNumericValue(character));
+    private boolean interpretCharacter() {
+        char character = code[codePointer.x];
+        return isStringMode ? interpretAsAscii(character) : interpretCommand(character);
+    }
+
+    private boolean interpretAsAscii(char character) {
+        if (character == '"')
+            isStringMode = !isStringMode;
+        else
+            stack.push((int) character);
+        return true;
+    }
+
+    private boolean interpretCommand(char character) {
         switch (character) {
             case '+':
                 add();
-                break;
+                return true;
             case '-':
                 subtract();
-                break;
+                return true;
             case '*':
                 multiply();
-                break;
+                return true;
             case '/':
                 divide();
-                break;
+                return true;
             case '%':
                 modulo();
-                break;
+                return true;
+            case '!':
+                logicalNot();
+                return true;
+            case '`':
+                greaterThan();
+                return true;
+            case '>':
+                return true;
+            case '<':
+                return true;
+            case '^':
+                return true;
+            case 'v':
+                return true;
+            case '?':
+                return true;
+            case '_':
+                return true;
+            case '|':
+                return true;
+            case '"':
+                isStringMode = !isStringMode;
+                return true;
+            case ':':
+                duplicateLast();
+                return true;
+            case '\\':
+                swapTop();
+                return true;
+            case '$':
+                stack.pop();
+                return true;
+            case '.':
+                outputAsInt();
+                return true;
+            case ',':
+                outputAsAscii();
+                return true;
+            case '#':
+                return true;
+            case 'p':
+                return true;
+            case 'g':
+                return true;
             case '@':
+                return false;
             default:
+                pushIfDigit(character);
+                return true;
         }
     }
 
@@ -67,8 +129,52 @@ public class BefungeInterpreter {
         stack.push(a == 0 ? 0 : b % a);
     }
 
-    public void clearStack() {
+    private void logicalNot() {
+        int value = stack.pop();
+        stack.push(value == 0 ? 1 : 0);
+    }
+
+    private void greaterThan() {
+        int a = stack.pop();
+        int b = stack.pop();
+        stack.push(b > a ? 1 : 0);
+    }
+
+    private void duplicateLast() {
+        if (stack.isEmpty())
+            stack.push(0);
+        else
+            stack.push(stack.peek());
+    }
+
+    private void swapTop() {
+        int a = stack.pop();
+        int b = stack.isEmpty() ? 0 : stack.pop();
+        stack.push(a);
+        stack.push(b);
+    }
+
+    private void outputAsInt() {
+        String integer = stack.pop().toString();
+        output.append(integer);
+    }
+
+    private void outputAsAscii() {
+        char character = (char) stack.pop().intValue();
+        output.append(character);
+    }
+
+    //-------------------------------------------------------
+
+    private void pushIfDigit(char character) {
+        if (Character.isDigit(character))
+            stack.push(Character.getNumericValue(character));
+    }
+
+    public void reset() {
         stack.clear();
+        codePointer.setLocation(0, 0);
+        output.setLength(0);
     }
 
     public Stack<Integer> getStack() {
--- tests/BefungeInterpreterTest.java
+++ tests/BefungeInterpreterTest.java
@@ -2,7 +2,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
+import java.util.Stack;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Timeout(value = 2)
 class BefungeInterpreterTest {
@@ -23,7 +26,7 @@ class BefungeInterpreterTest {
     void shouldPushDigitOnStack() {
         interpreter.run("0@");
         assertEquals(0, interpreter.getStack().peek());
-        interpreter.clearStack();
+        interpreter.reset();
         interpreter.run("9@");
         assertEquals(9, interpreter.getStack().peek());
     }
@@ -35,7 +38,7 @@ class BefungeInterpreterTest {
     }
 
     @Test
-    void shouldAddFourDigits(){
+    void shouldAddFourDigits() {
         interpreter.run("1111+++@");
         assertEquals(4, interpreter.getStack().peek());
     }
@@ -104,5 +107,148 @@ class BefungeInterpreterTest {
     void shouldPush0ForModulo0() {
         interpreter.run("50%@");
         assertEquals(0, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldPush1ForLogicalNotWith0() {
+        interpreter.run("0!@");
+        assertEquals(1, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldPush0ForLogicalNotWithNon0() {
+        interpreter.run("7!@");
+        assertEquals(0, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldPush1For5GreaterThan1() {
+        interpreter.run("51`@");
+        assertEquals(1, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldPush0For1LessThan5() {
+        interpreter.run("15`@");
+        assertEquals(0, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldPush0ForEqualNumbers() {
+        interpreter.run("11`@");
+        assertEquals(0, interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldPushAsciiCharacter() {
+        interpreter.run("\"a\"@");
+        assertEquals('a', interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldPushMultipleAsciiCharacters() {
+        interpreter.run("\"tset\"@");
+        Stack<Integer> stack = interpreter.getStack();
+        StringBuilder actual = new StringBuilder();
+        while (!stack.isEmpty())
+            actual.append((char) stack.pop().intValue());
+        assertEquals("test", actual.toString());
+    }
+
+    @Test
+    void shouldPushCommandAsAsciiCharacter() {
+        interpreter.run("\"@\"@");
+        assertEquals('@', interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldPutNumberAsAsciiCharacter() {
+        interpreter.run("\"2\"@");
+        assertEquals('2', interpreter.getStack().peek());
+    }
+
+    @Test
+    void shouldDuplicateLastValue() {
+        interpreter.run("5:@");
+        Stack<Integer> stack = interpreter.getStack();
+        assertEquals(2, stack.size());
+        assertEquals(5, stack.pop());
+        assertEquals(5, stack.pop());
+    }
+
+    @Test
+    void shouldPut0ForEmptyStackDuplication() {
+        interpreter.run(":@");
+        Stack<Integer> stack = interpreter.getStack();
+        assertEquals(1, stack.size());
+        assertEquals(0, stack.peek());
+    }
+
+    @Test
+    void shouldSwapTwoTopValues() {
+        interpreter.run("123\\@");
+        Stack<Integer> stack = interpreter.getStack();
+        assertEquals(3, stack.size());
+        assertEquals(2, stack.pop());
+        assertEquals(3, stack.pop());
+        assertEquals(1, stack.pop());
+    }
+
+    @Test
+    void shouldSwapSingleElementWithExtra0() {
+        interpreter.run("1\\@");
+        Stack<Integer> stack = interpreter.getStack();
+        assertEquals(2, stack.size());
+        assertEquals(0, stack.pop());
+        assertEquals(1, stack.pop());
+    }
+
+    @Test
+    void shouldPopAndDiscardSingleValue() {
+        String output = interpreter.run("1$@");
+        assertEquals(output, "");
+        assertTrue(interpreter.getStack().isEmpty());
+    }
+
+    @Test
+    void shouldPopAndOutputSingleValue() {
+        String output = interpreter.run("5.@");
+        assertEquals("5", output);
+    }
+
+    @Test
+    void shouldPopAndOutputMultipleValues() {
+        String output = interpreter.run("12345.....@");
+        assertEquals("54321", output);
+    }
+
+    @Test
+    void shouldOutputIntWithTwoDigits() {
+        String output = interpreter.run("99+.@");
+        assertEquals("18", output);
+    }
+
+    @Test
+    void shouldOutputCharAsInt() {
+        String output = interpreter.run("\"a\".@");
+        assertEquals(String.valueOf((int)'a'), output);
+    }
+
+    @Test
+    void shouldOutputAsciiCharacter() {
+        String output = interpreter.run("\"a\",@");
+        assertEquals("a", output);
+    }
+
+    @Test
+    void shouldOutputMultipleAsciiCharacters() {
+        String output = interpreter.run("\"12abc34\",,,,,,,@");
+        assertEquals("43cba21", output);
+    }
+
+    @Test
+    void shouldOutputIntAsChar() {
+        String output = interpreter.run("257**,@"); //7*5*2=70 ('F')
+        assertEquals("F", output);
     }
 }
